55937cac8abd16c5bc2c3f51fca1c07f43d3c1f7
focalsal <focalsal@cpanel9.v.fozzy.com>
2015-10-06 12:35:53 +0200

    category v1.0

--- component.php
+++ component.php
@@ -1,16 +1,46 @@
-<?
+<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+if( !\Bitrix\Main\Loader::includeModule('alfa4.chinavasion') ) 
+
+    die('не удалось найти компонент [alfa4.chinavasion]');
 
 $lifeTime = 30*60*24; 
  
 $cacheID = '_CAT_1'; 
 
-if($this->StartResultCache($lifeTime, $cacheID)) {
-  
- 
-  
+$cache = new CPHPCache;
+
+if($cache->StartDataCache($lifeTime, $cacheID) ) {
+   
+   $data_request = array(
+
+       "include_content" => "2"
+   );
+
+   $responce = requestChinavasion('getCategory', $data_request);
+
+   if($responce) {
+
+       $arResult =  $responce['categories'];
+
+   }
+
+    
+   $this->IncludeComponentTemplate();
+
+   $templateCachedData = $this->GetTemplateCachedData();
+
+   $cache->EndDataCache(
+      array(
+         "arResult" => $arResult,
+         "templateCachedData" => $templateCachedData
+      )
+   );
+
 } else {
 
-
+   extract($cache->GetVars());
+   $this->SetTemplateCachedData($templateCachedData);
 }
- $this->IncludeComponentTemplate();
+ 
 ?>
